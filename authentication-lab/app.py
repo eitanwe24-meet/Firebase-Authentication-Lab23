82c6625733106ed35e85a9f769200e2159beeec8
eitanwe24-meet <wertmaneitan@gmail.com>
2023-07-23 18:17:34 +0300

    finished

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,17 +2,51 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+
+config = {
+  "apiKey": "AIzaSyDG2Wte4WO76rYkOSgJTxWpQZVLBEwBscE",
+  "authDomain": "eitanw12345.firebaseapp.com",
+  "projectId": "eitanw12345",
+  "storageBucket": "eitanw12345.appspot.com",
+  "messagingSenderId": "85868299687",
+  "appId": "1:85868299687:web:566d3582ac4623b920a282",
+  "measurementId": "G-T3SXKGG9PN",
+  "databaseURL": ""
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authentication failed"
+
     return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    erorr = ""
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authentication failed  "
     return render_template("signup.html")
 
 
